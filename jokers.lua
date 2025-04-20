3e2ecb43a5bad9cb66a406fe767405ca156c374a
Glitchkat10 <136650292+kierkat10@users.noreply.github.com>
2025-04-20 16:00:52 -0700

    Add files via upload

--- jokers.lua
+++ jokers.lua
@@ -129,5 +129,24 @@ SMODS.Joker {
 	end
 }
 
+SMODS.Joker {
+	key = "joker_2",
+	config = { extra = { a_chips = 4 } },
+	rarity = 1,
+	atlas = "crp_jokers",
+	pos = { x = 7, y = 0 },
+	cost = 1,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.a_chips } }
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main then
+			return {
+				chips = card.ability.extra.a_chips
+			}
+		end
+	end
+}
+
 ----------------------------------------------
 ------------MOD CODE END----------------------
