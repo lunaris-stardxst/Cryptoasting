35385ec2c96ae2663e429936c8800d3b69dee2dc
Glitchkat10 <136650292+kierkat10@users.noreply.github.com>
2025-04-20 15:17:12 -0700

    Add files via upload

--- jokers.lua
+++ jokers.lua
@@ -5,7 +5,7 @@ SMODS.Atlas {
 	key = "jokers",
 	path = "atlas_jokers.png",
 	px = 71,
-	py = 71
+	py = 95
 }
 
 SMODS.Joker {
@@ -23,6 +23,34 @@ SMODS.Joker {
 		if context.joker_main then
 			return {
 				chips = card.ability.extra.a_chips
+			}
+		end
+	end,
+}
+
+SMODS.Joker {
+	key = "normalis",
+	config = { extra = { e_chips = 2.7, e_mult = 2.7 } },
+	rarity = "cry_exotic",
+	atlas =  "crp_jokers",
+	blueprint_compat = true,
+	pos = { x = 1, y = 0 },
+	soul_pos = { x = 2, y = 0, extra = { x = 3, y = 0 } },
+	cost = 50,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.e_chips, card.ability.extra.e_mult } }
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main then
+			return {
+				Echip_mod = card.ability.extra.e_chips,
+				Emult_mod = card.ability.extra.e_mult,
+				message = localize({
+					type = "variable",
+					key = "a_powmultchips",
+					vars = { card.ability.extra.e_chips },
+				}),
+				colour = { 0.8, 0.45, 0.85, 1 },
 			}
 		end
 	end,
