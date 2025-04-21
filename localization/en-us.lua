a76285c39eedd89c24b3bafc32a3eb01c3e63ea7
Ruby <138878489+lord-ruby@users.noreply.github.com>
2025-04-21 17:19:22 +0100

    tags & sprites

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -139,7 +139,7 @@ return {
 			k_crp_exomythic = "ExoMythic",
 			["k_crp_:3"] = ":3",
 			k_crp_cryptastic = "Cryptastic",
-			k_crp_exomythicepicawesomeuncommon2mexotic_22exomythic4mecipe = "ExoMythicEpicAwesomeUncommon2MExotic 22ExoMythic4meCipe",
+			k_crp_exomythicepicawesomeuncommon2mexotic_22exomythic4mecipe = "ExoMythicEpicAwesomeUncommon2MExotic 22ExoMythic4meCipeCryptasticRare 2",
 		}
 	}
 }
