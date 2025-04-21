09b45333a44a772b845c75a7eb026e38d848c261
Glitchkat10 <136650292+kierkat10@users.noreply.github.com>
2025-04-20 23:11:40 -0700

    add bulgoe to main menu

--- main.lua
+++ main.lua
@@ -2,3 +2,45 @@
 
 assert(SMODS.load_file('jokers.lua'))()
 assert(SMODS.load_file('tags.lua'))()
+
+local oldfunc = Game.main_menu
+Game.main_menu = function(change_context)
+	local ret = oldfunc(change_context)
+	-- adds bulgoe to the main menu
+	local newcard = Card(
+		G.title_top.T.x,
+		G.title_top.T.y,
+		G.CARD_W,
+		G.CARD_H,
+		G.P_CARDS.empty,
+		G.P_CENTERS.j_crp_bulgoe,
+		{ bypass_discovery_center = true }
+	)
+	-- recenter the title
+	G.title_top.T.w = G.title_top.T.w * 1.7675
+	G.title_top.T.x = G.title_top.T.x - 0.8
+	G.title_top:emplace(newcard)
+	-- make the card look the same way as the title screen Ace of Spades
+	newcard.T.w = newcard.T.w * 1.1 * 1.2
+	newcard.T.h = newcard.T.h * 1.1 * 1.2
+	newcard.no_ui = true
+	newcard.states.visible = false
+	G.E_MANAGER:add_event(Event({
+		trigger = "after",
+		delay = 0,
+		blockable = false,
+		blocking = false,
+		func = function()
+			if change_context == "splash" then
+				newcard.states.visible = true
+				newcard:start_materialize({ G.C.WHITE, G.C.WHITE }, true, 2.5)
+			else
+				newcard.states.visible = true
+				newcard:start_materialize({ G.C.WHITE, G.C.WHITE }, nil, 1.2)
+			end
+			return true
+		end,
+	}))
+
+	return ret
+end
