f36bf8aa32310ef26003c08cd424d49a350a0d00
Rainstar760 <121758006+Rainstar760@users.noreply.github.com>
2025-05-29 12:23:00 +0300

    i fixed duplicare 2's joker

--- items/joker.lua
+++ items/joker.lua
@@ -613,18 +613,26 @@ SMODS.Joker {
 	loc_vars = function(self, info_queue, card)
 		return { vars = { lenient_bignum(card.ability.extra.emult), lenient_bignum(card.ability.extra.emult_gain), lenient_bignum(card.ability.extra.retriggers) } }
 	end,
-	calculate = function(self, card, context)
-		if context.repetition then
-			if context.cardarea == G.play or context.cardarea == G.jokers then
-				card.ability.extra.emult = lenient_bignum(card.ability.extra.emult) + lenient_bignum(card.ability.extra.emult_gain)
-				return {
-					message = localize("k_again_ex"),
-					repetitions = lenient_bignum(card.ability.extra.retriggers),
-					card = card,
-				}
-			end
+	calculate = function(self, card, context) 
+		if context.retrigger_joker_check and not context.retrigger_joker and context.other_card ~= card then
+			return {
+				message = localize("k_again_ex"),
+				repetitions = lenient_bignum(card.ability.extra.retriggers),
+				card = card,
+			}
 		end
-		if (context.post_trigger and context.other_joker ~= card) or (context.individual and context.cardarea == G.play) then
+		if context.repetition and context.cardarea == G.play then
+			return {
+				message = localize("k_again_ex"),
+				repetitions = lenient_bignum(card.ability.extra.retriggers),
+				card = card,
+			}
+		end
+		if context.post_trigger and context.other_joker ~= card then
+			card.ability.extra.emult = lenient_bignum(card.ability.extra.emult) + lenient_bignum(card.ability.extra.emult_gain)
+			card_eval_status_text(card, "extra", nil, nil, nil, { message = localize("k_upgrade_ex") })
+		end
+		if context.individual and context.cardarea == G.play then
 			card.ability.extra.emult = lenient_bignum(card.ability.extra.emult) + lenient_bignum(card.ability.extra.emult_gain)
 			card_eval_status_text(card, "extra", nil, nil, nil, { message = localize("k_upgrade_ex") })
 		end
@@ -703,51 +711,6 @@ SMODS.Joker {
 		return { vars = { lenient_bignum(card.ability.extra.chipsmult), '{', '}' } }
 	end,
 	calculate = function(self, card, context)
-		-- jokers, doesnt work as of now
-	    --if context.other_card ~= self and context.cardarea == G.jokers then
-		--	local arrow_number_jokers = 0
-		--	for i = 1, #G.jokers.cards do
-		--		if context.other_card == G.jokers.cards[i] then
-		--			arrow_number_jokers = i
-		--		end
-		--	end
-		--	if context.post_trigger and context.other_card == G.jokers.cards[arrow_number_jokers] then
-		--		if arrow_number_jokers == 1 then
-		--			return {
-		--				message = localize({
-		--					type = "variable",
-		--					key = "a_powmultchips",
-		--					vars = { number_format(lenient_bignum(card.ability.extra.chipsmult)) },
-		--				}),
-		--				Echip_mod = card.ability.extra.chipsmult,
-		--				Emult_mod = card.ability.extra.chipsmult
-		--			}
-		--		elseif arrow_number_jokers == 2 then
-		--			return {
-		--				message = '^^' .. lenient_bignum(card.ability.extra.chipsmult) .. ' Chips & Mult',
-		--				colour = G.C.EDITION,
-		--				EEchip_mod = card.ability.extra.chipsmult,
-		--				EEmult_mod = card.ability.extra.chipsmult
-		--			}
-		--		elseif arrow_number_jokers == 3 then
-		--			return {
-		--				message = '^^^' .. lenient_bignum(card.ability.extra.chipsmult) .. ' Chips & Mult',
-		--				colour = G.C.EDITION,
-		--				EEEchip_mod = card.ability.extra.chipsmult,
-		--				EEEmult_mod = card.ability.extra.chipsmult
-		--			}
-		--		elseif arrow_number_jokers >= 4 then
-		--			return {
-		--				message = '{' .. arrow_number_jokers .. '} ' .. lenient_bignum(card.ability.extra.chipsmult) .. ' Chips & Mult',
-		--				colour = G.C.EDITION,
-		--				hyperchip_mod = {arrow_number_jokers, card.ability.extra.chipsmult},
-		--				hypermult_mod = {arrow_number_jokers, card.ability.extra.chipsmult}
-		--			}
-		--		end
-		--	end
-		--end
-
-		-- playing cards
 		local arrow_number_cards = 0
 		if context.individual and context.cardarea == G.play then
 			local arrow_number_cards = 1
