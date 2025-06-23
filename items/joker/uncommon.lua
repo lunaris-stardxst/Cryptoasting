584ee0be88c887a2a43265f4145fb9004812dd5a
Wilfredlam0418 <128967273+Wilfredlam0418@users.noreply.github.com>
2025-06-23 18:52:02 +0800

    Update uncommon.lua

--- items/joker/uncommon.lua
+++ items/joker/uncommon.lua
@@ -40,6 +40,51 @@ SMODS.Atlas {
 } ]]--
 
 SMODS.Joker {
+	key = "vermillion",
+	name = "Vermillion Joker",
+	pos = { x = 3, y = 0 },
+	config = { extra = { Xmult = 3 } },
+	rarity = 2,
+	cost = 6,
+	atlas = "crp_placeholders",
+	blueprint_compat = true,
+	demicoloncompat = true,
+	loc_vars = function(self, info_queue, card)
+		return { vars = { card.ability.extra.Xmult } }
+	end,
+	add_to_deck = function(self, card, from_debuff)
+		if not from_debuff then
+			for i=1, #G.jokers.cards do
+				eligible_cards = {}
+				if not G.jokers.cards[i] == card and not G.jokers.cards[i].ability.eternal then
+					eligible_cards[#eligible_cards+1] = G.jokers.cards[i]
+				end
+			end
+			if #eligible_cards > 0 then
+				local option = pseudorandom_element(eligible_cards, pseudoseed("crp_vermillion"))
+			end
+			for i=1, #G.jokers.cards do
+				if G.jokers.cards[i] == option then idx = i end
+			end
+			if idx and G.jokers.cards[idx] then
+				G.jokers.cards[idx]:start_dissolve()
+				G.jokers.cards[idx]:remove_from_deck()
+				SMODS.add_card({key = "j_joker"})
+			end
+		end
+	end,
+	calculate = function(self, card, context)
+		if context.joker_main or context.forcetrigger then
+			return { xmult = lenient_bignum(card.ability.extra.Xmult) }
+		end
+	end,
+	crp_credits = {
+		idea = { "Poker The Poker" },
+		code = { "wilfredlam0418" }
+	}
+}
+
+SMODS.Joker {
 	key = "joker_of_all_trades",
 	name = "Joker of all Trades",
 	config = { extra = { chips = 150, mult = 15, dollars = 3 } },
