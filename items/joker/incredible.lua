ad0ff6babcf47295ec58385aa0b81d160c7d02cf
Wilfredlam0418 <128967273+Wilfredlam0418@users.noreply.github.com>
2025-07-02 20:09:37 +0800

    sine

---
+++ items/joker/incredible.lua
@@ -0,0 +1,47 @@
+SMODS.Atlas {
+	key = "joker",
+	path = "atlas_joker.png",
+	px = 71,
+	py = 95
+}
+
+SMODS.Atlas {
+	key = "placeholder",
+	path = "atlas_placeholder.png",
+	px = 71,
+	py = 95
+}
+
+SMODS.Joker {
+	key = "sine_wave",
+	name = "Sine Wave",
+	config = {},
+	rarity = "crp_incredible",
+	atlas = "crp_placeholder",
+	pos = { x = 1, y = 1 },
+	cost = 15,
+	blueprint_compat = true,
+	demicoloncompat = true,
+	loc_vars = function()
+		local total_mult = 0
+		for i = 1, G.GAME.round do
+			total_mult = total_mult + math.sin(i) + 1
+		end
+		return { vars = { total_mult } }
+	end,
+	calculate = function(self, card, context)
+		if (context.joker_main) or context.forcetrigger then
+			local total_mult = 0
+			for i = 1, G.GAME.round do
+				total_mult = total_mult + math.sin(i) + 1
+			end
+			return {
+				mult = total_mult
+			}
+		end
+	end,
+	crp_credits = {
+		idea = { "wilfredlam0418" },
+		code = { "wilfredlam0418" }
+	}
+}
