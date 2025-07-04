56af59d7d630760a27f0c5c534e34faf5934c89e
Wilfredlam0418 <128967273+Wilfredlam0418@users.noreply.github.com>
2025-07-04 13:23:06 +0800

    4 pots

---
+++ items/consumable/pot.lua
@@ -0,0 +1,106 @@
+SMODS.Atlas {
+	key = "consumable",
+	path = "atlas_consumable.png",
+	px = 71,
+	py = 95
+}
+
+SMODS.Atlas {
+	key = "placeholder",
+	path = "atlas_placeholder.png",
+	px = 71,
+	py = 95
+}
+
+SMODS.ConsumableType {
+	key = "Pot",
+	primary_colour = { 1, 1, 1, 1 },
+	secondary_colour = { 0.75, 0.75, 0.75, 1 },
+	collection_rows = { 4, 4 },
+	shop_rate = 0,
+	loc_txt = {},
+	default = "c_crp_desires",
+	can_stack = true,
+	can_divide = true
+}
+
+SMODS.Consumable {
+	key = "desires",
+	name = "Pot of Desires",
+	set = "Pot",
+	pos = { x = 10, y = 2 },
+	config = { max_highlighted = 2 },
+	atlas = "crp_placeholder",
+	loc_vars = function(self, info_queue, card)
+		return { vars = { lenient_bignum(card.ability.max_highlighted) } }
+	end,
+	use = function()
+		for i = 1, #G.hand.highlighted do
+			G.hand.highlighted[i]:start_dissolve()
+			G.hand.highlighted[i]:remove_from_deck()
+		end
+	end,
+	crp_credits = {
+		idea = { "Psychomaniac14" },
+		code = { "wlfredlam0418" }
+	}
+}
+
+SMODS.Consumable {
+	key = "greed",
+	name = "Pot of Greed",
+	set = "Pot",
+	pos = { x = 10, y = 2 },
+	config = { extra = { cards = 3 } },
+	atlas = "crp_placeholder",
+	loc_vars = function(self, info_queue, card)
+		return { vars = { lenient_bignum(card.ability.extra.cards) } }
+	end,
+	can_use = function()
+		return true
+	end,
+	use = function(self, card)
+		SMODS.draw_cards(card.ability.extra.cards)
+	end,
+	crp_credits = {
+		idea = { "Psychomaniac14" },
+		code = { "wlfredlam0418" }
+	}
+}
+
+SMODS.Consumable {
+	key = "gold",
+	name = "Pot of Gold",
+	set = "Pot",
+	pos = { x = 10, y = 2 },
+	atlas = "crp_placeholder",
+	can_use = function()
+		return true
+	end,
+	use = function()
+		SMODS.add_card({ set = "Playing Card", enhancement = "m_gold" })
+	end,
+	crp_credits = {
+		idea = { "Psychomaniac14" },
+		code = { "wlfredlam0418" }
+	}
+}
+
+SMODS.Consumable {
+	key = "potluck",
+	name = "Potluck",
+	set = "Pot",
+	pos = { x = 10, y = 2 },
+	config = { max_highlighted = 1 },
+	atlas = "crp_placeholder",
+	loc_vars = function(self, info_queue, card)
+		return { vars = { lenient_bignum(card.ability.max_highlighted) } }
+	end,
+	use = function()
+		SMODS.add_card({ set = "Food" })
+	end,
+	crp_credits = {
+		idea = { "Psychomaniac14" },
+		code = { "wlfredlam0418" }
+	}
+}
