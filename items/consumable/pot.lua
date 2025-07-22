96067da0ce71ec109337f36de5a672d7edb3e864
Wilfredlam0418 <128967273+Wilfredlam0418@users.noreply.github.com>
2025-07-22 23:12:25 +0800

    add shuffle functions and pot of dichotomy

--- items/consumable/pot.lua
+++ items/consumable/pot.lua
@@ -10,6 +10,40 @@ SMODS.ConsumableType {
 	can_divide = true
 }
 
+-- Functions on shuffling cards back into the deck
+
+function shuffle(card_pos)
+	if (card_pos or 1) <= #G.hand.cards then
+		local cards = {}
+		for i, j in ipairs(G.hand.cards) do
+			cards[i] = j
+		end
+		for i = 1, (card_pos or 1) - 1 do
+			cards[i + 1] = G.hand.cards[i]
+		end
+		cards[1] = G.hand.cards[card_pos or 1]
+		G.hand.cards = cards
+		draw_card(G.hand, G.deck)
+	end
+end
+
+function shuffle_random(amount, key_append)
+	for _ = 1, amount or 1 do
+		shuffle(math.ceil(pseudorandom(key_append or "") * #G.hand.cards))
+	end
+end
+
+function shuffle_all_highlighted()
+	for i, j in ipairs(G.hand.highlighted) do
+		for i, k in ipairs(G.hand.cards) do
+			if j == k then
+				shuffle(i)
+				break
+			end
+		end
+	end
+end
+
 SMODS.Consumable {
 	key = "desires",
 	name = "Pot of Desires",
@@ -26,6 +60,29 @@ SMODS.Consumable {
 			G.hand.highlighted[i]:remove_from_deck()
 		end
 		SMODS.draw_cards(#G.hand.highlighted)
+	end,
+	crp_credits = {
+		idea = { "Psychomaniac14" },
+		code = { "wlfredlam0418" }
+	}
+}
+
+SMODS.Consumable {
+	key = "dichotomy",
+	name = "Pot of Dichotomy",
+	set = "Pot",
+	pos = { x = 10, y = 2 },
+	config = { immutable = { cards = 2 }, extra = { jokers = 1 } },
+	atlas = "crp_placeholder",
+	loc_vars = function(self, info_queue, card)
+		return { vars = { lenient_bignum(card.ability.immutable.cards), lenient_bignum(card.ability.extra.jokers) } }
+	end,
+	can_use = function(self, card)
+		return #G.hand.highlighted >= card.ability.immutable.cards
+	end,
+	use = function(self, card)
+		shuffle_random(2, "crp_dichotomy1")
+		SMODS.add_card({ set = "Joker", key_append = "crp_dichotomy2" })
 	end,
 	crp_credits = {
 		idea = { "Psychomaniac14" },
