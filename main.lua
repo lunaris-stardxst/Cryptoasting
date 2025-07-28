6dab4b5a8d555f87dd7673082fb6fb774d9aba34
triple6lexi <hiimlexilol@gmail.com>
2025-07-28 00:11:29 -0600

    fix
    fixed something that needed to be fixed

--- main.lua
+++ main.lua
@@ -58,3 +58,5 @@ local files = {
 for i, v in pairs(files) do
 	assert(SMODS.load_file(v..".lua"))()
 end
+
+-- https://gist.githubusercontent.com/MattIPv4/045239bc27b16b2bcf7a3a9a4648c08a/raw/2411e31293a35f3e565f61e7490a806d4720ea7e
